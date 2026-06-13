3e58642833b7492581c54f91eae3f2b7c03f66ac
ISRAEL <2303a51825@sru.edu.in>
2026-06-13 09:23:04 +0530

    fixed deployment bug

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -28,8 +28,8 @@ const ProductForm = ({addProducts}) => {
   return (
     <div>
         <form onSubmit={handleSubmit}>
-            <input type = "text" name = "title" placeholder='Enter Title' value={formdata.title} onChange={handleChange} />
-            <input type = "number" name = "price" placeholder=' Enter Price' value={formdata.price} onChange={handleChange} />
+            <input type = "text" name = "title" placeholder='Enter Title....' value={formdata.title} onChange={handleChange} />
+            <input type = "number" name = "price" placeholder=' Enter Price....' value={formdata.price} onChange={handleChange} />
             <input type = "text" name = "description" placeholder='Enter Description' value={formdata.description} onChange={handleChange} />
             <input type = "text" name = "category" placeholder='Enter Category' value={formdata.category} onChange={handleChange} />
             <input type = "text" name = "image" placeholder='Enter Image URL' value={formdata.image} onChange={handleChange} />
